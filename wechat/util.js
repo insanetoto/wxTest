a78ab978b92480b93e4b41276a0a23c57e5d605c
ZhangXin <insanetomato@me.com>
2016-05-10 14:38:44 +0800

    bugfix

--- wechat/util.js
+++ wechat/util.js
@@ -2,7 +2,7 @@
 var xml2js = require('xml2js')
 var Promise = require('bluebird')
 
-export.parseXMLAsync = function(xml){
+exports.parseXMLAsync = function(xml){
 	return new Promise(function(resolve ,reject){
 		xml2js.parseString(xml,{trim:true},function(error,content){
 			if(err) reject (err)
