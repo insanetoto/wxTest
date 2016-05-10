90d40e3af00811096e9a665a84ec9d5032019bc4
ZhangXin <insanetomato@me.com>
2016-05-10 09:39:09 +0800

    抽象个中间件出来

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var debug = require('debug')('wxTest');
 var koa = require('koa');
 var sha1 = require('sha1')
+var wechat = require('./wechat/g')
 //配置文件
 var config = require('./config/config');
 var wxconfig = {
@@ -12,27 +13,7 @@ var wxconfig = {
 }
 
 var app = koa();
-app.use(function *(next){
-    //config 注入中间件，方便调用配置信息
-    if(!this.config){
-        this.config = config;
-    }
-    var token = wxconfig.wechat.token
-    var signature = this.query.signature
-    var nonce = this.query.nonce
-    var timestamp = this.query.timestamp
-    var echostr = this.query.echostr
-    var str = [token , timestamp, nonce].sort().join('')
-    var sha = sha1(str)
-    if (sha === signature){
-	this.body = echostr + ''
-	console.log(echostr)
-    }
-    else{
-	this.body = 'wrong'
-    }
-    yield next;
-});
+app.use(wechat(weconfig.wechat))
 app.listen(config.port);
 console.log('listening on port %s',config.port);
 module.exports = app;
---
+++ wechat/g.js
@@ -0,0 +1,29 @@
+use 'strict'
+
+var sha1 = require('sha1')
+//配置文件
+var wxconfig = {
+    wechat:{
+	appID:'wx752f2bc195410378',
+	appSecret:'f1c7e28b84827391d60ec2aa0b5c2f55',
+	token:'mydearhuangwen' 
+    }
+}
+
+
+module.exports = function(opts){
+    return  function *(next){        
+        var token = opts.token
+        var signature = this.query.signature
+        var nonce = this.query.nonce
+        var timestamp = this.query.timestamp;
+        var echostr = this.query.echostr
+        var str = [token,timestamp,nonce].sort().join('')
+        var sha = sha1(str)
+        if(sha === signature){
+            this.body = echostr +''
+        }else{
+            this.body = 'wrong'
+        }
+    }
+}
