e5d62b6a5001e27cc98282d94d91896ac67c0e19
zhangxin <insanetomato@me.com>
2016-05-11 13:03:50 +0800

    1
    1

--- wechat/g.js
+++ wechat/g.js
@@ -39,7 +39,7 @@ module.exports = function(opts){
 
             if(message.MsgType === 'event'){
                 if ( message.Event === 'subscribe'){
-                    console.log('here')
+
                     var now = new Date().getTime()
 
                     that.status = 200
@@ -51,6 +51,7 @@ module.exports = function(opts){
                                  '<MsgType><![CDATA[text]]></MsgType>'+
                                  '<Content><![CDATA[你好]]></Content>'+
                                  '</xml>'
+                    console.log(that.body)
                     return
                 }
             }
