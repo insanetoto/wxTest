b1861898c48e894e176d9c08990a7d53cf957b02
zhangxin <insanetomato@me.com>
2016-05-12 13:46:01 +0800

    1
    1

--- wechat/wechat.js
+++ wechat/wechat.js
@@ -26,7 +26,7 @@ function Wechat(opts){
             }
 
             if (that.isValidAccessToken(data)){
-                Promise.resolve(data)
+                return Promise.resolve(data)
             }
             else {
                 return that.updateAccessToken()
