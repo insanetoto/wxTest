1702232b4781fb7f6fc43392f816966bf8984fa7
zhangxin <insanetomato@me.com>
2016-05-12 15:14:36 +0800

    1
    1

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict'
 var path = require('path')
-var util = require('./lib/utils')
+var util = require('./libs/utils')
 var wechat_file = path.join(__dirname , './config/wechat_file')
 var wxconfig = {
     wechat:{
