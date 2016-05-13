d408ee19ff406808923dfdd19db43e32e0b64650
zhangxin <insanetomato@me.com>
2016-05-13 13:50:37 +0800

    1
    1

--- weixin.js
+++ weixin.js
@@ -3,12 +3,14 @@
 exports.reply = function* (next){
 	var message = this.weixin
 
+	console.log("here ----------------weixin.js ")
 	if (message.MsgType === 'event'){
 		if(message.Event === 'subscribe'){
 			if (message.EventKey){
 				console.log('扫描二维码:'+ EventKey+' '+ message.ticket)
 			}
 			this.body ="欢迎订阅\r\n"+ '消息ID:'+ message.MsgId
+			console.log("here ----------------weixin.js  关注 ")
 
 		}
 	}
